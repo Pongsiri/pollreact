ec3e7d3bdbc287b24bb85a8029a2c8bc093c33aa
pongsiri <jostakhun@gmail.com>
2017-07-18 00:03:56 +0700

    init

---
+++ app-questions.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        "q": "คิดว่าการแข่งขันทราวเวอร์ระหว่างพี่โขพี่จอสครั้งนี้ความยุติธรรมกี่เปอร์เซ็น",
+        "a": "100%",
+        "b": "80%",
+        "c": "50%",
+        "d": "0%"
+    },
+    {
+        "q": "How much money would you like to make?",
+        "a": "no money",
+        "b": "some money",
+        "c": "most of the money",
+        "d": "all of the money"
+    },
+    {
+        "q": "How much money would you like to put down?",
+        "a": "more money than I have",
+        "b": "all of my money",
+        "c": "some of my money",
+        "d": "no money at all"
+    },
+    {
+        "q": "Who do you want to be your boss?",
+        "a": "someone bossy",
+        "b": "someone who knows what they are doing",
+        "c": "I'm my own boss",
+        "d": "no boss at all"
+    }
+];
